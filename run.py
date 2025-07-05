f602b3a00722e269cfa7273fd935e43401f592b7
Your Name <you@example.com>
2025-07-05 18:07:58 +0530

    Removed invalid package and updated app/init and run files

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
-# run.py
+from app import create_app
 
-from flask import Flask
-from app.routes import main
-
-app = Flask(__name__)
-app.register_blueprint(main)
+app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
